e15b726be87c173242271cc6d6132a3332fdcf49
Angelo <angelomarchionni@inwind.it>
2020-04-01 14:46:41 +0200

    salvataggioFormLocale

--- src/app/pages/pagamento/pagamento.page.ts
+++ src/app/pages/pagamento/pagamento.page.ts
@@ -50,6 +50,17 @@ export class PagamentoPage implements OnInit {
   totaleCompresoTrasporto:string;
   totaleCompresoTrasportoNumero:number;
 
+  nomeSalvato:string;
+  cognomeSalvato:string;
+  indirizzoSalvato:string;
+  cittaSalvata:string;
+  statoSalvato:string;
+  telefonoSalvato:string;
+  codicePostaleSalvato:string;
+  emailSalvata:string;
+
+
+
   countries: Array<CountryPhone>;
   genders: Array<string>;
 
@@ -64,21 +75,34 @@ export class PagamentoPage implements OnInit {
     //  We just use a few random countries, however, you can use the countries you need by just adding them to this list.
     // also you can use a library to get all the countries from the world.
     console.log("sono nell init " + window.localStorage.getItem('citta'));
-    /*
-    this.validations_form.setValue({nome:[window.localStorage.getItem('nome')],
-      lastname: [window.localStorage.getItem('lastname')],
-      citta: [window.localStorage.getItem('citta')],
-      codicePostale: [window.localStorage.getItem('codicepostale')],
-      country: [window.localStorage.getItem('codicepostale')] 
-      });
-      */
-     /*
-     this.validations_form.setValue({
-       nome:[window.localStorage.getItem('nome')],
-      
-      citta: [window.localStorage.getItem('citta')]
-      });
-      */
+if(window.localStorage.getItem('nome'))
+    this.nomeSalvato = window.localStorage.getItem('nome');
+if(window.localStorage.getItem('lastname'))   
+    this.cognomeSalvato = window.localStorage.getItem('lastname');
+if(window.localStorage.getItem('indirizzo'))  
+    this.indirizzoSalvato = window.localStorage.getItem('indirizzo');
+if(window.localStorage.getItem('citta'))  
+    this.cittaSalvata = window.localStorage.getItem('citta');
+if(window.localStorage.getItem('stato'))     
+    this.statoSalvato = window.localStorage.getItem('stato');
+if(window.localStorage.getItem('telefono'))  
+    this.telefonoSalvato = window.localStorage.getItem('telefono');
+if(window.localStorage.getItem('codicepostale'))  
+    this.codicePostaleSalvato = window.localStorage.getItem('codicepostale');
+if(window.localStorage.getItem('email'))     
+    this.emailSalvata =  window.localStorage.getItem('email');
+   
+    if (window.localStorage.getItem('stato') =='IT')
+    this.prezzoTrasporto =10.02; 
+    else if 
+    (window.localStorage.getItem('stato') =='DE')
+    this.prezzoTrasporto =7.02;
+  
+      console.log("stato" + window.localStorage.getItem('stato'));
+      console.log("prezzo trasp" + this.prezzoTrasporto);
+      // this.totaleCompresoTrasportoNumero= this.prezzoTrasporto+this.totaleDaPagareNumero;
+     
+    
     
     this.totaleCompresoTrasportoNumero=0;
     this.countries = [
@@ -90,7 +114,7 @@ export class PagamentoPage implements OnInit {
         this.totaleDaPagareNumero = +this.totaleDaPagare;
         console.log(this.totaleDaPagare);
       });
-      this.prezzoTrasporto=0;
+      // this.prezzoTrasporto=0;
       this.totaleCompresoTrasporto=(this.prezzoTrasporto+this.totaleDaPagareNumero).toFixed(2);
       this.totaleCompresoTrasportoNumero=(this.prezzoTrasporto+this.totaleDaPagareNumero);
       
@@ -139,17 +163,27 @@ export class PagamentoPage implements OnInit {
       //   Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
       //   Validators.required
       // ])),
-      name: new FormControl('', Validators.required),
-      lastname: new FormControl('', Validators.required),
-      indirizzo: new FormControl('', Validators.required),
-      citta: new FormControl('', Validators.required),
-      country: new FormControl('', Validators.required),
-      codicePostale: new FormControl('', Validators.required),
-      phone: new FormControl('', Validators.compose([
+      /*
+      window.localStorage.setItem('nome',this.validations_form.get('name').value);
+ window.localStorage.setItem('lastname',this.validations_form.get('lastname').value);
+ window.localStorage.setItem('indirizzo',this.validations_form.get('indirizzo').value);
+ window.localStorage.setItem('codicepostale',this.validations_form.get('codicePostale').value);
+ window.localStorage.setItem('citta',this.validations_form.get('citta').value);
+ window.localStorage.setItem('stato',this.validations_form.get('country').value);
+ window.localStorage.setItem('telefono',this.validations_form.get('phone').value);
+ window.localStorage.setItem('email',this.validations_form.get('email').value);
+ */
+      name: new FormControl(this.nomeSalvato, Validators.required),
+      lastname: new FormControl(this.cognomeSalvato, Validators.required),
+      indirizzo: new FormControl(this.indirizzoSalvato, Validators.required),
+      citta: new FormControl(this.cittaSalvata, Validators.required),
+      country: new FormControl(this.statoSalvato, Validators.required),
+      codicePostale: new FormControl(this.codicePostaleSalvato, Validators.required),
+      phone: new FormControl(this.telefonoSalvato, Validators.compose([
         Validators.required,
         Validators.pattern(/^-?(0|[1-9]\d*)?$/)
       ])),
-      email: new FormControl('', Validators.compose([
+      email: new FormControl(this.emailSalvata, Validators.compose([
          Validators.required,
          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
        ])),
